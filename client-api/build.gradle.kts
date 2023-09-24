34453a024ffb6f38e3ca428ad5f42b284ee78c78
Miles <59733060+MilesGlitch@users.noreply.github.com>
2023-09-24 15:55:00 +0300

    upload

---
+++ client-api/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `java-library`
+}
+
+group = "ru.cristalix"
+
+dependencies {
+    api("org.lwjgl.lwjgl", "lwjgl_util", "2.9.3")
+    api("io.netty", "netty-buffer", "4.1.58.Final")
+    api("com.mojang", "authlib", "2.1.28")
+    api("dev.xdark", "feder", "live-SNAPSHOT")
+    api("dev.xdark", "clientapi", "2.0.15")
+    api("com.google.guava", "guava", "21.0")
+    api("com.google.code.gson", "gson", "2.8.6")
+    api("it.unimi.dsi", "fastutil", "8.3.1")
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("clientApi") {
+            groupId = "ru.cristalix"
+            artifactId = "client-api"
+            version = "6.0.0"
+            from(components["java"])
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "cristalix-client-sdk"
 
 include("ui-engine", "bundler-gradle-plugin", "client-sdk")
+include("client-api")
